7e4f8153be65957775fa63039883d375cdbc7880
marioweid <mario.weidner@gmx.de>
2024-09-27 17:20:21 +0200

    added fe be communication

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,64 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello from the Go backend!")
-	})
-
-	// Start the server
-	fmt.Println("Server started at :8080")
-	http.ListenAndServe(":8080", nil)
+type Beer struct {
+	Name  string `json:"name"`
+	Price string `json:"price"`
 }
+
+var albums = []Beer{
+	{Name: "Augustiner Hell", Price: "56.99€"},
+	{Name: "Giesinger", Price: "17.99€"},
+}
+
+// handler, normally database request
+func getBeers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/api/beers", getBeers)
+	router.Run("0.0.0.0:8080")
+}
+
+// package main
+
+// import (
+//     "github.com/gin-gonic/gin"
+// )
+
+// func main() {
+//     // Create a new Gin router
+//     r := gin.Default()
+
+//     // Create a route group with the /api prefix
+//     apiGroup := r.Group("/api")
+//     {
+//         apiGroup.GET("/users", func(c *gin.Context) {
+//             c.JSON(200, gin.H{
+//                 "message": "List of users",
+//             })
+//         })
+
+//         apiGroup.POST("/users", func(c *gin.Context) {
+//             c.JSON(201, gin.H{
+//                 "message": "User created",
+//             })
+//         })
+
+//         apiGroup.GET("/posts", func(c *gin.Context) {
+//             c.JSON(200, gin.H{
+//                 "message": "List of posts",
+//             })
+//         })
+//     }
+
+//     // Start the server
+//     r.Run() // By default, it will listen and serve on 0.0.0.0:8080
+// }
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	interface Beer {
+		name: string;
+		price: string;
+	}
+    let disabled = false;
+	let promise: Promise<Beer[]> = Promise.resolve([]);
+
+	async function fetchBeers(): Promise<Beer[]> {
+		const response = await fetch('/api/beers', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		if (response.ok) {
+            return response.json();
+		} else {
+            throw new Error('Failed to fetch data from backend');
+        }
+	}
+
+    function handleClick(){
+        promise = fetchBeers();
+        disabled = true;
+    }
+</script>
+
+<button
+	class="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+	on:click={() => handleClick()}
+    { disabled }
+>
+	<span class="mx-1">Fetch Beers from Go-Backend</span>
+</button>
+
+{#await promise}
+	<p>Loading</p>
+{:then beers}
+    {#if beers.length > 0}
+        <p><b>Beer</b> - <b>Price</b></p>    
+    {/if}
+	{#each beers as beer}
+		<p>{beer.name} - {beer.price}</p>
+	{/each}
+{:catch error}
+	<p>Error happened! {error}</p>
+{/await}
